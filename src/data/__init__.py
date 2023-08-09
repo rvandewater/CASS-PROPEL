1da01a0360a743f2a6f35835ed9dd3e35fe490f1
Bjarne Pfitzner <41118143+BjarnePfitzner@users.noreply.github.com>
2023-08-09 11:20:18 +0200

    Removed broken Pancreas Data import

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,12 +1,12 @@
 from .esophagus.esophagus_data import EsophagusData
-from .pancreas.pancreas_data import PancreasData
+# from .pancreas.pancreas_data import PancreasData
 from .stomach.stomach_data import StomachData
 
 
 def get_data_from_name(name):
     if name == 'esophagus':
         return EsophagusData()
-    if name == 'pancreas':
-        return PancreasData()
+    # if name == 'pancreas':
+    #     return PancreasData()
     if name == 'stomach':
         return StomachData()
